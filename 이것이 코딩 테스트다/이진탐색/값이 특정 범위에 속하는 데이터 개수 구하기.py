c45f4895ad8156d95552b60140bf0919b67d6ad8
springhana <xksxksanfro7@naver.com>
2024-04-29 23:14:41 +0900

    코딩 테스트

--- 이것이 코딩 테스트다/이진탐색/값이 특정 범위에 속하는 데이터 개수 구하기.py
+++ 이것이 코딩 테스트다/이진탐색/값이 특정 범위에 속하는 데이터 개수 구하기.py
@@ -1,6 +1,5 @@
 from bisect import bisect_left, bisect_right
 
-
 # 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
 def count_by_range(a, left_value, right_value):
     right_index = bisect_right(a, right_value)
